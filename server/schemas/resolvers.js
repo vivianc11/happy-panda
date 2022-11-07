2c44368e69bc0177ad8b2f221d860dba06da7b44
Rosebud <rkle408@gmail.com>
2022-11-07 13:04:08 -0800

    added some query and mutation to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,14 +1,42 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Thought } = require('../models');
+const { User, Reflection } = require('../models');
 const { signToken } = require('../utils/auth');
+
+// will need to add more queries and mutations once we finalize models and schemas
 
 const resolvers = {
     Query: {
-
+        users: async () => {
+            return User.find()
+        },
+          user: async (parent, { username }) => {
+            return User.findOne({ username });
+        },
     },
 
     Mutation: {
-
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw new AuthenticationError('No user found with this email address');
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const token = signToken(user);
+      
+            return { token, user };
+          },
     },
 };
 
