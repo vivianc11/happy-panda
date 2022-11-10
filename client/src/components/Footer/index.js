c3de424f47d519d7c7482ec2dff185d3a251fc1a
Rosebud <rkle408@gmail.com>
2022-11-10 10:53:58 -0800

    adjusted footer to stay at bottom, updated README resources

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,7 +3,7 @@ import 'bulma/css/bulma.min.css';
 
 const Footer = () => {
   return (
-    <footer className="hero-foot">
+    <footer className="hero-foot" style={{position: "absolute", bottom: "0"}}>
       <div className="content has-text-centered is-flex-align-items-flex-end">
         <br></br>
         <br></br>
